19cac7708928a717e521daa78584dec1ed275462
Oscar Hjelm <>
2020-05-28 16:23:29 +0200

    Initial.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # word-autoqa
+
+A simple python3-script which uses the rules specified under `rules/`, e.g. `generic.json` and checks whether or not they are present in the Word DOCX document file supplied as the script argument.
+
+Rules in the rule folder may be split into multiple files; the script will use all files.
---
+++ word-autoqa.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+try:
+    from xml.etree.cElementTree import XML
+except ImportError:
+    from xml.etree.ElementTree import XML
+import zipfile
+import json
+import re
+import argparse
+from os import listdir
+from os.path import isfile, join, dirname
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def get_docx_text(path):
+    WORD_NAMESPACE = '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}'
+    PARA = WORD_NAMESPACE + 'p'
+    TEXT = WORD_NAMESPACE + 't'
+
+    try:
+        document = zipfile.ZipFile(path)
+        xml_content = document.read('word/document.xml')
+        document.close()
+    except:
+        print("[ ! ] Error, could not open file", path)
+        exit(1)
+
+    tree = XML(xml_content)
+
+    paragraphs = []
+    for paragraph in tree.getiterator(PARA):
+        texts = [node.text
+                 for node in paragraph.getiterator(TEXT)
+                 if node.text]
+        if texts:
+            paragraphs.append(''.join(texts))
+
+    return '\n\n'.join(paragraphs)
+
+
+def get_rules(path):
+    rules = []
+    files = [f for f in listdir(path) if isfile(join(path, f))]
+    for file in files:
+        try:
+            with open(path+file) as f:
+                contents = f.read()
+                rules += json.loads(contents)["rules"]
+        except:
+            print("[ ! ] Could not process file", path+file, ", skipping...")
+
+    return rules
+
+
+def validate(text, rules):
+    passed = failed = 0
+
+    for rule in rules:
+        match = re.search(rule["find"], text)
+        if match:
+            failed += 1
+            print(bcolors.FAIL, rule["fail-message"], "(\"" + match.group() + "\")", bcolors.ENDC)
+        else:
+            passed += 1
+            #print(bcolors.OKGREEN, rule["pass-message"], bcolors.ENDC)
+
+    print("[ + ] Finished.", passed, "rules passed,", failed, "failed.")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Validate document text against a set of rules.")
+    parser.add_argument("file", help='Name of DOCX file')
+    args = parser.parse_args()
+    print("[ + ] Running validation rules against file", args.file)
+
+    text = get_docx_text(args.file)
+    rules = get_rules(join(dirname(__file__), "rules/"))
+    validate(text, rules)
